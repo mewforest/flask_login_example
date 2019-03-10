e9737dcacbeff00000ceb4629ed712f357e697a8
mewforest <mew.forest@gmail.com>
2019-03-10 12:42:08 +0300

    Исправил: ошибки с формой логина Добавил: форму отправки файла (небезопасно)

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_from_directory
 from scheme import User
+from werkzeug.utils import secure_filename
+import os
 
 
 app = Flask(__name__)
 admin = User('admin', '123')
 all_users = [admin]
 
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSION = 'jpg'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 @app.route('/')
 def index():
-    if 'current_user' in session:
+    if 'current_login' in session:
         return render_template('logged.html')
+    else:
+        return redirect(url_for('login_page'))
+
+
+@app.route('/go')
+def go_form():
+    if 'current_login' in session:
+        return render_template('go_form.html')
     else:
         return redirect(url_for('login_page'))
 
@@ -24,11 +40,37 @@ def register_page():
     return render_template('login.html', mode='REGISTER')
 
 
+@app.route('/go', methods=['POST'])
+def go():
+# check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and '.jpg' in file.filename:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+   
+ 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+                               
+                               
+
 @app.route('/auth', methods=['POST'])
 def auth():
     login_user = User(request.form['login'], request.form['password'])
     if login_user in all_users:
-        session['current_user'] = login_user
+        session['current_login'] = login_user.username
         return redirect(url_for('index'))
     else:
         return render_template('error.html')
@@ -36,7 +78,7 @@ def auth():
 
 @app.route('/logout')
 def logout():
-    session.pop('current_user', None)
+    session.pop('current_login', None)
     return redirect(url_for('index'))
 
 
--- scheme.py
+++ scheme.py
@@ -1,14 +1,14 @@
 class User:
     def __init__(self, login, password, session=None):
-        self.login = login
+        self.username = login
         self.password = password
         self.session = session
 
     def __eq__(self, other):
-        if self.login == other.login and self.password == other.password:
+        if self.username == other.username and self.password == other.password:
             return True
         else:
             return False
 
     def __repr__(self):
-        return self.login
+        return 'User: ' + self.username
